3976a2055bf2452e2ed4f4955dddcc4990530d5a
Domashov Oleg <domashov@gmail.com>
2020-01-08 20:44:13 +0300

    Arr Folder

---
+++ arr/scrypt.js
@@ -0,0 +1,8 @@
+const Items = [
+    {name: 'qwe', price: 100},
+    {name: 'sdf', price: 200},
+    {name: 'rdf', price: 10},
+    {name: 'gggg', price: 5},
+    {name: '1f4', price: 500}
+]
+console.log(Items)
